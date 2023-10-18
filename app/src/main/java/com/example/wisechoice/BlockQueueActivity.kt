895e7b0e163f7ff81b2537a5b4f2c35c6a0f99bc
unknown <israttasnimesha1046@gmail.com>
2023-10-18 23:02:15 +0600

    esha 4

---
+++ app/src/main/java/com/example/wisechoice/BlockQueueActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wisechoice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BlockQueueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_block_queue)
+    }
+}
--- app/src/main/java/com/example/wisechoice/MinerTransactionActivity.kt
+++ app/src/main/java/com/example/wisechoice/MinerTransactionActivity.kt
@@ -1,18 +1,35 @@
 package com.example.wisechoice
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 
-class MinerTransactionActivity : AppCompatActivity() {
+class MinerTransactionActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     var st_phone: String? = null
-
     var miner_bottom: BottomNavigationView? = null
 
     var addToBlockFragment: AddToBlockFragment = AddToBlockFragment()
     var addTransactionFragment: AddTransactionFragment = AddTransactionFragment()
     var mineFragment: MineFragment = MineFragment()
+
+    var username: TextView? = null
+    var phone: TextView? = null
+    var photo: ImageView? = null
+    var home_menu: ImageView? = null
+
+    var drawerLayout: DrawerLayout? = null
+    var navigationView: NavigationView? = null
+    var nView: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,24 +42,65 @@ class MinerTransactionActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(R.id.m_frame, addTransactionFragment)
             .commit()
 
-        miner_bottom?.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item ->
-            if (item.itemId == R.id.m_add_transaction) {
-                supportFragmentManager.beginTransaction().replace(R.id.m_frame, addTransactionFragment)
-                    .commit()
-            }
-            if (item.itemId == R.id.m_add_to_block) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.m_frame, addToBlockFragment).commit()
-            }
-            if (item.itemId == R.id.m_mine) {
-                supportFragmentManager.beginTransaction().replace(R.id.m_frame, mineFragment)
-                    .commit()
+        miner_bottom?.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.m_add_transaction -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.m_frame, addTransactionFragment)
+                        .commit()
+                }
+                R.id.m_add_to_block -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.m_frame, addToBlockFragment)
+                        .commit()
+                }
+                R.id.m_mine -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.m_frame, mineFragment)
+                        .commit()
+                }
             }
             true
-        })
+        }
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer)
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this, drawerLayout,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        actionBarDrawerToggle.syncState()
+
+        navigationView = findViewById<NavigationView>(R.id.navigation)
+        nView = navigationView?.getHeaderView(0)
+        username = nView?.findViewById<TextView>(R.id.username)
+        phone = nView?.findViewById<TextView>(R.id.phone)
+        photo = nView?.findViewById<ImageView>(R.id.photo)
+        home_menu = findViewById<ImageView>(R.id.home_menu)
+
+        home_menu?.setOnClickListener {
+            drawerLayout?.openDrawer(GravityCompat.START)
+        }
+
+        navigationView?.setNavigationItemSelectedListener(this)
     }
 
     companion object {
         const val SHARED_PREF_NAME = "myPref"
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.block_queue -> {
+                val intent2 = Intent(this, BlockQueueActivity::class.java)
+                startActivity(intent2)
+            }
+            R.id.blockchain -> {
+                val intent2 = Intent(this, BlockchainActivity::class.java)
+                startActivity(intent2)
+            }
+            R.id.logout -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finish the current activity
+            }
+        }
+        return true
     }
 }
